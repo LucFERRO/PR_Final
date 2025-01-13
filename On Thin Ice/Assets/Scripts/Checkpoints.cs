85e0823c9c32a6cb8b61716ad2767a854d3afefd
Luc FERRO <luchenri.ferro@gmail.com>
2025-01-13 19:30:07 +0100

    respawn if fall in water

--- On Thin Ice/Assets/Scripts/Checkpoints.cs
+++ On Thin Ice/Assets/Scripts/Checkpoints.cs
@@ -2,49 +2,82 @@ using UnityEngine;
 
 public class Checkpoints : MonoBehaviour
 {
-    public Transform player;
+    public Transform playerTransform;
     public Transform defaultPosition;
-    public Transform[] targets;
+    public Transform[] targetsTransforms;
+    public Transform waterTransform;
+    public float waterYCheat;
 
+    public float[] cpDistances;
+    private int closestCheckpointIndex;
 
     private void Start()
     {
-        player.position = defaultPosition.position;
+        playerTransform.position = defaultPosition.position;
     }
 
     void Update()
     {
-        
+        ClosestCheckpoint();
+        FallInWater();
+        ManageCheckpointInputs();
+    }
+    void ManageCheckpointInputs()
+    {
         if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha8))
         {
-            TeleportPlayer(0); 
+            TeleportPlayer(0);
         }
-        else if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha9))
+        if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha9))
         {
-            TeleportPlayer(1); 
+            TeleportPlayer(1);
         }
-        else if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha0))
+        if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha0))
         {
-            TeleportPlayer(2); 
+            TeleportPlayer(2);
         }
-        else if (Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Alpha4))
+        if (Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Alpha4))
         {
             TeleportPlayer(3);
         }
-        else if (Input.GetKeyDown(KeyCode.Keypad5) || Input.GetKeyDown(KeyCode.Alpha5))
+        if (Input.GetKeyDown(KeyCode.Keypad5) || Input.GetKeyDown(KeyCode.Alpha5))
         {
             TeleportPlayer(4);
         }
-        else if (Input.GetKeyDown(KeyCode.Keypad6) || Input.GetKeyDown(KeyCode.Alpha6))
+    }
+    void FallInWater()
+    {
+        if (playerTransform.position.y <= waterTransform.position.y + waterYCheat)
         {
-            TeleportPlayer(5);
+            TeleportPlayer(closestCheckpointIndex);
         }
+    }
+
+    void ClosestCheckpoint()
+    {
+        int chosenIndex = 0;
+        Vector3 playerTransformWithoutY = playerTransform.transform.position;
+        playerTransformWithoutY.y = 0;        
+        Vector3 ChosenCheckpointTransformWithoutY = targetsTransforms[chosenIndex].transform.position;
+        ChosenCheckpointTransformWithoutY.y = 0;
+
+        for (int i = 0; i < targetsTransforms.Length; i++)
+        {
+            Vector3 checkpointTransformWithoutY = targetsTransforms[i].transform.position;
+            checkpointTransformWithoutY.y = 0;
+            cpDistances[i] = Vector3.Distance(playerTransformWithoutY, checkpointTransformWithoutY);
+
+            float distToCheckpoint = Vector3.Distance(playerTransformWithoutY, ChosenCheckpointTransformWithoutY);
+            if (distToCheckpoint >= cpDistances[i])
+            {
+                chosenIndex = i;
+            }
+        }
+        closestCheckpointIndex = chosenIndex;
     }
 
     void TeleportPlayer(int targetIndex)
     {
-     
-            player.position = targets[targetIndex].position; 
-      
+        playerTransform.position = targetsTransforms[targetIndex].position;
     }
 }
